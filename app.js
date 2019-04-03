6a18dc1ccf30fb8147140af7a8a4bf3719f215a3
Shanay Murdock <murdock.shanay@gmail.com>
2019-04-02 23:24:22 -0500

    Added event handling for random dice image and set initial scores to 0

--- app.js
+++ app.js
@@ -12,17 +12,38 @@ GAME RULES:
 var scores;
 var roundScore;
 var activePlayer;
-var dice;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 
-var x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // Do something here
+    // 1. Random Number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    // 3. Update roundScore only if the rolled number was NOT a 1
+
+})
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// var x = document.querySelector('#score-0').textContent;
