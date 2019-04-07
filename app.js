ba8ac64e5676e733b5252e16d57d1492bd1a5237
Shanay Murdock <murdock.shanay@gmail.com>
2019-04-07 14:30:52 -0500

    bug fix in init function

--- app.js
+++ app.js
@@ -87,8 +87,9 @@ function init() {
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    document.getElementById('#name-0').textContent = 'Player-1';
-    document.getElementById('#name-1').textContent = 'Player-2';
+    document.getElementById('name-0').textContent = 'Player-1';
+    document.getElementById('name-1').textContent = 'Player-2';
+
     document.querySelector('.player-0-panel').classList.remove('winner');
     document.querySelector('.player-1-panel').classList.remove('winner');
     document.querySelector('.player-0-panel').classList.remove('active');
